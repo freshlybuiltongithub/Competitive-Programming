780e9b0f1a93501129fced0af74a2e877e6d38b0
vishal sharma <vishalsh533@gmail.com>
2020-10-22 13:35:27 +0530

    Added 30 days of code solution (#21)

---
+++ HackerRank/30 Days of Code/Python 3/Day 1: Data Types.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+a = int(input())
+b = float(input())
+c = input() 
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(a+i)
+# Print the sum of the double variables on a new line.
+print(b + d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+c)
+
+
